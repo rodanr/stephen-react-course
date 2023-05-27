3ad0f37c64c2b21e43c3f25bd761f8cc0eaeb2c8
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-05-27 17:55:33 +0545

    feat: added notes to convert html to jsx

--- section2-creating-content-with-jsx/src/App.jsx
+++ section2-creating-content-with-jsx/src/App.jsx
@@ -1,6 +1,47 @@
 /* eslint-disable react/prop-types */
 import "./App.css";
 
+/**
+ * ----------------------------------------------------------------------------
+ *                        Converting HTML to JSX
+ * ----------------------------------------------------------------------------
+ * 1. All prop names follow camelCase
+ *
+ * example:
+ *
+ *      HTML -> <input maxlength="5"/>
+ *      JSX -> <input maxLength="5"/> (throws console error, can't pass "5" like this)
+ *
+ * 2. Number attributes use curly braces
+ *
+ * example:
+ *
+ *      HTML -> <input maxlength="5"/>
+ *      JSX -> <input maxLength={5}/> (This will work)
+ *
+ * 3. Boolean 'true' can be written with just the property name. 'False' should
+ * be written with curly braces
+ *
+ * example:
+ *
+ *      HTML -> <input spellcheck="true"/> <input spellcheck="false"/>
+ *      JSX -> <input spellCheck/> (optional to pass {true}) <input spellCheck={false}/>
+ *
+ * 4. The 'class' attribute is written as 'className'
+ *
+ * example:
+ *
+ *      HTML -> <div class="divider"/>
+ *      JSX -> <div className="divider"/>
+ *
+ * 5. In-line styles are provided as objects
+ * example:
+ *
+ *      HTML -> <div style="text-decoration: 'none'; padding: '5px';"/>
+ *      JSX -> <div style={{textDecoration: 'none', padding: '5px'/>
+ * ----------------------------------------------------------------------------
+ */
+
 // taking props inputType, minValue and style
 const InputField = ({ inputType, minValue, style }) => (
   <input type={inputType} min={minValue} style={style} />
