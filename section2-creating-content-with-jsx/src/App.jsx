dec7079bef75549e40503224aff3eac0bd04d8d6
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-05-27 17:18:02 +0545

    feat: working with variables in jsx done

---
+++ section2-creating-content-with-jsx/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+export default function App() {
+  // computed data or values
+  const name = "Samantha";
+  const age = 23;
+
+  // components to show
+  return (
+    <h1>
+      Hi, my name is {name} and my age is {age}
+    </h1>
+  );
+}
