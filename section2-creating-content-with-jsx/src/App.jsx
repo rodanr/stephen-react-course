3677a4141a1e972f37ba13ff10f94b9836e4ec96
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-05-27 17:38:06 +0545

    feat: working with props

--- section2-creating-content-with-jsx/src/App.jsx
+++ section2-creating-content-with-jsx/src/App.jsx
@@ -1,14 +1,26 @@
+/* eslint-disable react/prop-types */
 import "./App.css";
+
+// taking props inputType, minValue and style
+const InputField = ({ inputType, minValue, style }) => (
+  <input type={inputType} min={minValue} style={style} />
+);
 
 export default function App() {
   // computed data or values
   const name = "Samantha";
   const age = 23;
+  const style = {
+    border: "1px solid red",
+  };
 
   // components to show
   return (
-    <h1>
-      Hi, my name is {name} and my age is {age}
-    </h1>
+    <>
+      <h1>
+        Hi, my name is {name} and my age is {age}
+      </h1>
+      <InputField inputType={"number"} minValue={5} style={style} />
+    </>
   );
 }
