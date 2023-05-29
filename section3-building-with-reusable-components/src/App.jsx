898bb38bb991efd102d95ca79fbee9ffcb6c8c34
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-05-29 11:35:02 +0545

    section3: start

---
+++ section3-building-with-reusable-components/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import ProfileCard from "./components/ProfileCard";
+
+function App() {
+  return (
+    <div>
+      <div>Personal Digital Assistants</div>
+      <ProfileCard />
+      <ProfileCard />
+      <ProfileCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ section3-building-with-reusable-components/src/components/ProfileCard.jsx
@@ -0,0 +1,3 @@
+export default function ProfileCard() {
+  return <div>Profile Card !!!</div>;
+}
