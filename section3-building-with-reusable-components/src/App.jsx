b9c3af26c32d6399b1727b030e8660afabafd5a9
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-05-30 17:51:45 +0545

    section3: done with simple project to explore images and description as props and also use styling using balma

--- section3-building-with-reusable-components/src/App.jsx
+++ section3-building-with-reusable-components/src/App.jsx
@@ -1,13 +1,49 @@
 import "./App.css";
+import "bulma/css/bulma.css";
+
 import ProfileCard from "./components/ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
 function App() {
   return (
     <div>
-      <div>Personal Digital Assistants</div>
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana32" />
-      <ProfileCard title="Siri" handle="@siri01" />
+      <div className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assistants</p>
+        </div>
+      </div>
+      <div className="container">
+        <div className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa99"
+                image={AlexaImage}
+                description="Alexa was created by amazon and helps you buy things"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana32"
+                image={CortanaImage}
+                description="Cortana was made by Microsoft. Who knows what it does ?"
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Siri"
+                handle="@siri01"
+                image={SiriImage}
+                description="Siri was made by Apple and is being phased out"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- section3-building-with-reusable-components/src/components/ProfileCard.jsx
+++ section3-building-with-reusable-components/src/components/ProfileCard.jsx
@@ -1,8 +1,19 @@
 /* eslint-disable react/prop-types */
-export default function ProfileCard({ title, handle }) {
+export default function ProfileCard({ title, handle, image, description }) {
   return (
-    <div>
-      <div>Title is {title}</div> <div>Handle is{handle}</div>
+    <div className="card">
+      <div className="card-image">
+        <figure className="image is-1by1">
+          <img src={image} alt="pda logo" />
+        </figure>
+      </div>
+      <div className="card-content">
+        <div className="media-content">
+          <p className="title is-4">Title is {title}</p>{" "}
+          <p className="subtitle is-6">Handle is{handle}</p>
+        </div>
+        <div className="content">{description}</div>
+      </div>
     </div>
   );
 }
