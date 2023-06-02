924bfe4c648ac5a04145d73f4f6aab32343c890c
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-06-02 09:54:36 +0545

    section7: reusing same code of section 7 and persist book datas with api calls using jsonserver

---
+++ section6&7-how-to-handle-forms/books/src/App.jsx
@@ -0,0 +1,59 @@
+import "./App.css";
+import { useState, useEffect } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import axios from "axios";
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    const updatedBooks = [...books, response.data];
+
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+    const updatedBooks = books.filter((book) => book.id !== id);
+
+    setBooks(updatedBooks);
+  };
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) return { ...book, ...response.data };
+      return book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
+}
+
+export default App;
