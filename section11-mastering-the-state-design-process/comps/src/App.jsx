650d59940129427a9561b2e7a386e365fe03483e
Rodan Ramdam <rodan.ramdam@gmail.com>
2023-06-03 16:10:20 +0545

    section11: done, used project structure to hold components and pages, created accordion and used states to expand the items and used styling

---
+++ section11-mastering-the-state-design-process/comps/src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import Accordion from "./components/Accordion";
+// import ButtonPage from "./pages/ButtonPage";
+
+function App() {
+  const items = [
+    {
+      id: "dadc877a-494a-4fa6-a5f4-4836a831c038",
+      label: "Can I use React on a project ?",
+      content:
+        "You can use React on any project you want.You can use React on any project you want.You can use React on any project you want.You can use React on any project you want.",
+    },
+    {
+      id: "6c3989ae-1e2a-4205-aa38-976086225eb4",
+      label: "Can I use Javascript on a project?",
+      content:
+        "You can use Javascript on any project you want.You can use Javascript on any project you want.You can use Javascript on any project you want.",
+    },
+    {
+      id: "402a8b4a-7051-44ad-b80a-5a14b0c68365",
+      label: "Can I use CSS on a project?",
+      content:
+        "You can use CSS on any project you want.You can use CSS on any project you want.You can use CSS on any project you want.",
+    },
+  ];
+
+  return (
+    <>
+      <Accordion items={items} />
+    </>
+  );
+}
+
+export default App;
